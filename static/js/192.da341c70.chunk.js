72aaff351f806b17ac6fd9d4bd60c3810de06050
Olivia Huang <olivia.huang@sunbirddcim.com>
2025-04-01 15:53:46 +0800

    Updates

---
+++ static/js/192.da341c70.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkmy_ecommerce=self.webpackChunkmy_ecommerce||[]).push([[192],{192:(s,c,e)=>{e.r(c),e.d(c,{default:()=>g});var r=e(315),d=e(579);const g=()=>{const{id:s}=(0,r.g)();return(0,d.jsxs)("div",{className:"grid grid-cols-1 sm:grid-cols-2 md:grid-cols-2 lg:grid-cols-4 gap-6 sm:gap-8 md:gap-12 lg:gap-16 xl:gap-20",children:["ProductDetails with ProductID: ",s]})}}}]);
+//# sourceMappingURL=192.da341c70.chunk.js.map
