d37a372486b6cb2488c7dc6adfcbc3411fe305f3
oliviah11 <yycc11.tt@gmail.com>
2025-11-22 13:23:39 +0800

    remove unused constants

--- src/components/filters/filterOptions.jsx
+++ src/components/filters/filterOptions.jsx
@@ -1,63 +1,5 @@
-// sizes
-export const sizes = [
-  { code: 'xs', label: 'Extra Small' },
-  { code: 'sm', label: 'Small' },
-  { code: 'md', label: 'Medium' },
-  { code: 'lg', label: 'Large' },
-  { code: 'xl', label: 'Extra Large' },
-  // shoes
-  { code: '4', label: 'Size 4' },
-  { code: '4.5', label: 'Size 4.5' },
-  { code: '5', label: 'Size 5' },
-  { code: '5.5', label: 'Size 5.5' },
-  { code: '6', label: 'Size 6' },
-  { code: '6.5', label: 'Size 6.5' },
-  { code: '7', label: 'Size 7' },
-  { code: '7.5', label: 'Size 7.5' },
-  { code: '8', label: 'Size 8' },
-  { code: '8.5', label: 'Size 8.5' },
-  { code: '9', label: 'Size 9' },
-  { code: '9.5', label: 'Size 9.5' },
-  { code: '10', label: 'Size 10' },
-  { code: '10.5', label: 'Size 10.5' },
-  { code: '11', label: 'Size 11' },
-  { code: '11.5', label: 'Size 11.5' },
-  { code: '12', label: 'Size 12' },
-];
-
-// colors
-export const colors = [
-  'white',
-  'black',
-  'red',
-  'orange',
-  'yellow',
-  'green',
-  'blue',
-  'brown',
-  'beige',
-  'pink',
-];
-
-// ratings
 export const ratings = [5, 4, 3, 2, 1];
 
-// category_id
-export const categories = [
-  { id: 'unisex', label: 'Unisex' },
-  { id: 'women', label: 'Women' },
-  { id: 'men', label: 'Men' },
-];
-
-// Collection
-export const DEFAULT_COLLECTIONS = [
-  { collection_id: 'latest', name: 'Latest Arrivals' },
-  { collection_id: 'cozy', name: 'Cozy Comfort' },
-  { collection_id: 'urban', name: 'Urban Oasis' },
-  { collection_id: 'fresh', name: 'Fresh Fusion' },
-];
-
-// Filter key
 export const FILTER_KEYS = {
   COLLECTIONS: 'collection',
   CATEGORY: 'category',
@@ -65,3 +7,11 @@ export const FILTER_KEYS = {
   COLORS: 'colors',
   RATINGS: 'ratings',
 };
+
+// Collections
+export const DEFAULT_COLLECTIONS = [
+  { collection_id: 'latest', name: 'Latest Arrivals' },
+  { collection_id: 'cozy', name: 'Cozy Comfort' },
+  { collection_id: 'urban', name: 'Urban Oasis' },
+  { collection_id: 'fresh', name: 'Fresh Fusion' },
+];
