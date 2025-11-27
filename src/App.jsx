89ae7fce25230a18c0e0fc5b899077cd8390fe1e
Olivia Huang <olivia.huang@sunbirddcim.com>
2025-11-27 17:17:28 +0800

    update to fix redirect and publish issues

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
-import React, { lazy, Suspense } from "react";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import "./index.css"; 
+import React, { lazy, Suspense } from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import './index.css';
 
 const ProductDetails = lazy(() => import('./pages/ProductDetails'));
 const ProductListing = lazy(() => import('./pages/ProductListing'));
@@ -11,9 +11,8 @@ function App() {
       <div className="min-h-screen bg-gray-100 flex justify-start items-start p-4">
         <Suspense fallback={<div>Loading...</div>}>
           <Routes>
-            <Route path="/" element={<ProductListing />} />
             <Route path="/product/:id" element={<ProductDetails />} />
-            <Route path="/products" element={<ProductListing />} />
+            <Route path="/product-listing" element={<ProductListing />} />
           </Routes>
         </Suspense>
       </div>
@@ -22,4 +21,3 @@ function App() {
 }
 
 export default App;
-
