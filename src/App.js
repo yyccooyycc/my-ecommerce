084d32149d48ad4c158bf47fe93560ad3a046a6e
Olivia Huang <olivia.huang@sunbirddcim.com>
2025-12-01 17:15:37 +0800

    refactor-remove unused LatestArrivals

--- src/App.js
+++ src/App.js
@@ -1,16 +1,16 @@
-import React from "react";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import LatestArrival from "./pages/LatestArrivals";
-import ProductDetails from "./pages/ProductDetails";
-import ProductListing from "./pages/ProductListing";
-import "./index.css";
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import HomePage from './pages/HomePage';
+import ProductDetails from './pages/ProductDetails';
+import ProductListing from './pages/ProductListing';
+import './index.css';
 
 function App() {
   return (
     <Router basename="/my-ecommerce">
       <div className="min-h-screen bg-gray-100 flex justify-start items-start p-4">
         <Routes>
-          <Route path="/latest-arrivals" element={<LatestArrival />} />
+          <Route path="/" element={<HomePage />} />
           <Route path="/product/:id" element={<ProductDetails />} />
           <Route path="/product-listing" element={<ProductListing />} />
         </Routes>
